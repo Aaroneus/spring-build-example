b45cbabb3eaa23250f1e83512e2540f681f54f4d
Aaroneus <aprovis@mylifedigital.co.uk>
2019-06-10 13:43:50 +0100

    Initial commit.

---
+++ src/main/java/io/aaroneus/buildexample/BuildexampleApplication.java
@@ -0,0 +1,13 @@
+package io.aaroneus.buildexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildexampleApplication.class, args);
+	}
+
+}
